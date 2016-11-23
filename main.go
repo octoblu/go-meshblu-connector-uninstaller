76c8e36af6b2c203e031e5c54ce08cbda9d815d4
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-11-23 07:56:07 -0700

    Move user-service uninstall to a subcommand

--- main.go
+++ main.go
@@ -12,31 +12,40 @@ import (
 	De "github.com/visionmedia/go-debug"
 )
 
-var debug = De.Debug("go-meshblu-connector-uninstaller:main")
+var debug = De.Debug("meshblu-connector-uninstaller:main")
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "go-meshblu-connector-uninstaller"
-	app.UsageText = "go-meshblu-connector-uninstaller [options] <uuid>"
+	app.Name = "meshblu-connector-uninstaller"
+	app.UsageText = "meshblu-connector-uninstaller <command> [options] <uuid>"
 	app.Version = version()
-	app.Action = run
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:   "home-dir, H",
-			Usage:  "Home directory of the user",
-			EnvVar: "HOME",
-		},
-		cli.StringFlag{
-			Name:   "username, u",
-			Usage:  "System Username",
-			EnvVar: "USER",
+	app.Commands = []cli.Command{
+		{
+			Name:      "user-service",
+			Usage:     "uninstall a connector installed as a UserService",
+			ArgsUsage: "<uuid>",
+			Action:    UserService,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "home-dir, H",
+					Usage:  "Home directory of the user.",
+					EnvVar: "HOME",
+				},
+				cli.StringFlag{
+					Name:   "username, u",
+					Usage:  "System Username",
+					EnvVar: "USER",
+				},
+			},
 		},
 	}
+	app.Flags = []cli.Flag{}
 	app.Run(os.Args)
 }
 
-func run(context *cli.Context) {
-	homeDir, username, uuid := getOpts(context)
+// UserService uninstalls a connector installed as a UserService
+func UserService(context *cli.Context) {
+	homeDir, username, uuid := getUserServiceOpts(context)
 	err := manage.Uninstall(&manage.UninstallOptions{
 		UUID:            uuid,
 		HomeDir:         homeDir,
@@ -48,22 +57,33 @@ func run(context *cli.Context) {
 	os.Exit(0)
 }
 
-func getOpts(context *cli.Context) (string, string, string) {
+func anyFlagsMissing(context *cli.Context, flagNames ...string) bool {
+	for _, flagName := range flagNames {
+		if !context.IsSet(flagName) {
+			return false
+		}
+	}
+	return true
+}
+
+func getUserServiceOpts(context *cli.Context) (string, string, string) {
 	homeDir := context.String("home-dir")
 	username := context.String("username")
 	uuid := context.Args().Get(0)
 
-	if homeDir == "" || username == "" || uuid == "" {
+	if uuid == "" || anyFlagsMissing(context, "home-dir", "username") {
 		cli.ShowAppHelp(context)
 
-		if homeDir == "" {
-			color.Red("  Missing required option --home-dir, -H, env: HOME")
+		fmt.Println()
+
+		if !context.IsSet("home-dir") {
+			color.Red("Missing required option --home-dir, -H, env: HOME")
 		}
-		if username == "" {
-			color.Red("  Missing required option --username, -u, env: USER")
+		if !context.IsSet("username") {
+			color.Red("Missing required option --username, -u, env: USER")
 		}
 		if uuid == "" {
-			color.Red("  Missing required argument <uuid>")
+			color.Red("Missing required argument <uuid>")
 		}
 		os.Exit(1)
 	}
